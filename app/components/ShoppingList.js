d2dff43898d4740e46f15d201ceb12cd213f7580
Jonas Maeyens <jonas.maeyens@gmail.com>
2017-11-22 19:20:23 +0100

    zlkjvdqc
    blabla

---
+++ app/components/ShoppingList.js
@@ -0,0 +1,31 @@
+
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    ScrollView,
+    TouchableOpacity
+} from 'react-native';
+
+
+export default class ShoppingList extends React.Component {
+    constructor(){
+        super(props);
+        this.state = {
+            Title: '',
+            Items: []
+        }
+    }
+
+  render() {
+    return (
+        <View>
+
+        </View>  
+    );
+
+  }
+}
+
